cad9bd71f9fce9fed953005ac9b0df215dc047a2
sso-yul <ssoyul987@gmail.com>
2023-06-12 15:22:08 +0900

    팀원과 병합

--- OTTT/src/main/webapp/resources/js/mypage/message.js
+++ OTTT/src/main/webapp/resources/js/mypage/message.js
@@ -138,7 +138,6 @@ $(document).ready(function() {
 	    $("#msg-write").attr("data-url", url) // 답장 버튼에 URL을 저장
 	
 	    var sendUserNk = $(this).siblings(".msg-nicknm").text()
-	    $("#msg-number").text(msgNo)
 	    $("#msgNick").text(sendUserNk)
 	})
 
